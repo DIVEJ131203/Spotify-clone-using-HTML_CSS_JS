fa233a0690fb5090935823d5fedd0cf8ec42cc0a
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-21 18:34:44 +0530

    Spotify Clone Working

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Welcome to Spotify");;
+let songIndex=0;
+let audioElement=new Audio('Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3');
+let masterplay=document.getElementById('materplay');
+let myProgressBar=document.getElementById("myProgressBar")
+let songs =
+[
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
+    
+]
+// audioElement.play();
+masterplay.addEventListener('click',()=>
+{
+if(audioElement.paused||audioElement.currentTime<=0)
+{
+audioElement.play();
+}
+});
+document.addEventListener('timeupdate',()=>{
+console.log('timeupdate');
+});
