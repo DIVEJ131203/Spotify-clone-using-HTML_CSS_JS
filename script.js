a5ac2ed7204eae42ac3e97d9cde7e06ba10e4154
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2024-01-13 18:36:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let songIndex=0;
 let audioElement=new Audio('Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3');
 let masterplay=document.getElementById('materplay');
 let myProgressBar=document.getElementById("myProgressBar")
+
 let songs =
 [
     {songName:"Arjan Vailly",filePath:"Arjan-Vailly-X-Panga-(Remix)(PaglaSongs).mp3",coverPath:"https://tse2.mm.bing.net/th?id=OIP.Q43WOi5cNFnInO7maQ7-SQHaHa&pid=Api&P=0&h=180"},
